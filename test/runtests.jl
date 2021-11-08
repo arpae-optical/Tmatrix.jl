d446c394778a06af69996a7e5f5bc6b75b1c57db
Alok Singh <alokbeniwal@gmail.com>
2021-11-08 15:28:43 -0800

    rm extra

--- test/runtests.jl
+++ test/runtests.jl
@@ -63,6 +63,7 @@ function ∂objective_function(r_array, θ_array)
     )
 end
 
+# TODO increase wavelens
 const wl_or_freq_input = 1e-6
 const input_unit = "m"
 const n_max = 1
@@ -88,6 +89,7 @@ println("Starting optimization for T-matrix ...")
 
 for n_iteration in 1:50
     loss_here = objective_function(r_array, θ_array)
+    println("loss here: $loss_here")
     ∂loss_r, ∂loss_θ = ∂objective_function(r_array, θ_array)
     # TODO: zero out the first and last control points angle
     ∂loss_θ[1] = 0.0
@@ -123,132 +125,3 @@ for n_iteration in 1:50
     #     "cache/iteration_particle_plots/maximizing_emissivity/particle_geom_iteration_$(n_iteration).png",
     # )
 end
-
-using Tmatrix
-using Zygote
-using Plots
-
-function objective_function(r_array_input, θ_array_input)
-    # println(r_array)
-    # println("I am <<objective_function>>, and the type of r_array is $(typeof(r_array))")
-    # println("r_array = $r_array")
-
-    # quadrupling_mesh
-    r_θ_array = Tmatrix.quadruple_mesh_density(r_array_input, θ_array_input)
-    r_array = r_θ_array[:, 1]
-    θ_array = r_θ_array[:, 2]
-    println(r_array)
-    println(θ_array)
-    ϕ_array = zeros(size(θ_array))
-
-    T = Tmatrix.T_matrix_SeparateRealImag_arbitrary_mesh(
-        n_max,
-        wl_or_freq_input,
-        input_unit,
-        Eps_r_r_1,
-        Eps_r_i_1,
-        Mu_r_r_1,
-        Mu_r_i_1,
-        Eps_r_r_2,
-        Eps_r_i_2,
-        Mu_r_r_2,
-        Mu_r_i_2,
-        (collect(r_array)),
-        θ_array,
-        ϕ_array,
-        rotationally_symmetric,
-        symmetric_about_plane_perpendicular_z,
-        BigFloat_precision,
-    )
-    k1_complex = Tmatrix.get_WaveVector(
-        wl_or_freq_input;
-        input_unit = input_unit,
-        Eps_r = Complex(Eps_r_r_1, Eps_r_i_1),
-        Mu_r = Complex(Mu_r_r_1, Mu_r_i_1),
-    )
-
-    # println(Tmatrix.get_OrentationAv_scattering_CrossSection_from_Tmatrix(T, k1_complex))
-    # TODO blows up without the coefficient.
-    surface_area = Tmatrix.calculate_surface_area_of_axisymmetric_particle(r_array, θ_array)
-    emiss =
-        10^-13 *
-        Tmatrix.get_OrentationAv_emissivity_from_Tmatrix(T, k1_complex, surface_area)
-    return emiss # if we need to minimize scattering cross section
-
-    """
-    return -1 * Tmatrix.get_OrentationAv_emissivity_from_Tmatrix(
-        T,
-        Complex(k1_r, k1_i),
-        Tmatrix.calculate_surface_area_of_axisymmetric_particle(r_array, θ_array)
-    ) # if we need to minimize emissivity
-    """
-end
-
-function ∂objective_function(r_array, θ_array)
-    # println("I am <<∂objective_function>>, and the type of r_array is $(typeof(r_array))")
-    # println("r_array = $r_array")
-    return Zygote.gradient(
-        (r_array, θ_array) -> objective_function(r_array, θ_array),
-        (collect(r_array)),
-        (collect(θ_array)),
-    )
-end
-
-const wl_or_freq_input = 1e-6
-const input_unit = "m"
-const n_max = 1
-const Eps_r_r_1 = 1.0
-const Eps_r_i_1 = 0.0
-const Eps_r_r_2 = 1.5
-const Eps_r_i_2 = 0.01
-const Mu_r_r_1 = 1.0
-const Mu_r_i_1 = 0.0
-const Mu_r_r_2 = 1.0
-const Mu_r_i_2 = 0.0
-const n_θ_points = 10
-const rotationally_symmetric = true
-const symmetric_about_plane_perpendicular_z = false
-const BigFloat_precision = nothing
-const learning_rate = 0.5
-
-θ_array = collect(LinRange(1e-6, pi - 1e-6, n_θ_points))
-r_array = 0.5 * wl_or_freq_input * ones(size(θ_array))
-loss_array = []
-
-println("Starting optimization for T-matrix ...")
-
-for n_iteration in 1:50
-    loss_here = objective_function(r_array, θ_array)
-    ∂loss_r, ∂loss_θ = ∂objective_function(r_array, θ_array)
-    global r_array = r_array .- learning_rate .* ∂loss_r
-    global θ_array = θ_array .- learning_rate .* ∂loss_θ
-    append!(loss_array, loss_here)
-
-    println()
-    println("iteration #$n_iteration: loss_here = $loss_here, ∂loss = $∂loss_r, $∂loss_θ")
-    println("r_array = $r_array")
-
-    xyz = vcat(
-        Tmatrix.convert_coordinates_Sph2Cart.(r_array, θ_array, zeros(size(r_array)))...,
-    )
-    p1 = plot!(
-        xyz[:, 1],
-        xyz[:, 3],
-        aspect_ratio = :equal,
-        label = "particle after $n_iteration iterations",
-        title = "scattering cross section = $loss_here m",
-    )
-    p2 = plot(
-        1:length(loss_array),
-        loss_array,
-        xlabel = "iteration #",
-        ylabel = "scattering cross section (m)",
-    )
-    fig = plot(p1, p2, layout = (1, 2), size = (1200, 800))
-    mkpath("cache/iteration_particle_plots/maximizing_emissivity")
-    # savefig(
-    #     fig,
-    #     "cache/iteration_particle_plots/maximizing_emissivity/particle_geom_iteration_$(n_iteration).png",
-    # )
-end
-
